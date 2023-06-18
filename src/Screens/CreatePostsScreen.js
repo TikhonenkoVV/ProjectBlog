bd8b32c6d14ed197b73c10da85658494469f7b7f
TikhonenkoVV <113926273+TikhonenkoVV@users.noreply.github.com>
2023-06-19 02:13:32 +0300

    add camera

--- src/Screens/CreatePostsScreen.js
+++ src/Screens/CreatePostsScreen.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect, useState, useRef } from "react";
 import {
     StyleSheet,
     Pressable,
@@ -13,64 +13,169 @@ import {
 } from "react-native";
 import { SvgXml } from "react-native-svg";
 import { BtnStyled } from "../Components/BtnStyled";
-import { iconCamera, iconMarker, iconRecicle } from "../../assets/img/icons";
+import {
+    iconCamera,
+    iconCameraExit,
+    iconCameraFlip,
+    iconCameraSnap,
+    iconMarker,
+    iconRecicle,
+} from "../../assets/img/icons";
+import { Camera } from "expo-camera";
+import * as MediaLibrary from "expo-media-library";
+import { TouchableOpacity } from "react-native";
+import { Dimensions } from "react-native";
+import { StatusBar } from "react-native";
+import { useNavigation } from "@react-navigation/native";
 
 export const CreatePostsScreen = () => {
+    const [hasPermission, setHasPermission] = useState(null);
+    const [cameraRef, setCameraRef] = useState(null);
+    const [type, setType] = useState(Camera.Constants.Type.back);
+    const [showCamera, setShowCamera] = useState(true);
+    const [photo, setPhoto] = useState(null);
+
+    const navigation = useNavigation();
+
+    useEffect(() => {
+        (async () => {
+            const { status } = await Camera.requestCameraPermissionsAsync();
+            await MediaLibrary.requestPermissionsAsync();
+            setHasPermission(status === "granted");
+        })();
+    }, []);
+
+    if (hasPermission === null) {
+        return <View />;
+    }
+    if (hasPermission === false) {
+        return <Text>No access to camera</Text>;
+    }
+
+    const takePhoto = async () => {
+        if (cameraRef) {
+            const { uri } = await cameraRef.takePictureAsync();
+            await MediaLibrary.createAssetAsync(uri);
+            setPhoto(uri);
+            setShowCamera(false);
+        }
+    };
+
     const onPublic = () => {};
 
     return (
-        <>
-            <TouchableWithoutFeedback onPress={Keyboard.dismiss}>
-                <View style={styles.container}>
-                    <View style={styles.imageWrapper}>
-                        <Pressable style={styles.camera}>
-                            <SvgXml xml={iconCamera} />
-                        </Pressable>
-                        <Image style={styles.image} />
+        <TouchableWithoutFeedback onPress={Keyboard.dismiss}>
+            <View style={styles.container}>
+                {showCamera && (
+                    <View style={styles.cameraContainer}>
+                        <Camera
+                            style={styles.camera}
+                            type={type}
+                            ref={setCameraRef}
+                        ></Camera>
+                        <View style={styles.btnWrapper}>
+                            <TouchableOpacity
+                                onPress={() => {
+                                    setShowCamera(false);
+                                }}
+                            >
+                                <SvgXml xml={iconCameraExit} />
+                            </TouchableOpacity>
+                            <TouchableOpacity
+                                style={styles.button}
+                                onPress={takePhoto}
+                            >
+                                <SvgXml xml={iconCameraSnap} />
+                            </TouchableOpacity>
+                            <TouchableOpacity
+                                onPress={() => {
+                                    setType(
+                                        type === Camera.Constants.Type.back
+                                            ? Camera.Constants.Type.front
+                                            : Camera.Constants.Type.back
+                                    );
+                                }}
+                            >
+                                <SvgXml xml={iconCameraFlip} />
+                            </TouchableOpacity>
+                        </View>
                     </View>
-                    <Text style={styles.imageTitle}>Завантажте фото</Text>
+                )}
+                <View style={styles.imageWrapper}>
+                    <TouchableOpacity
+                        onPress={() => setShowCamera(true)}
+                        style={styles.btnCamera}
+                    >
+                        <SvgXml xml={iconCamera} />
+                    </TouchableOpacity>
+                    <Image source={{ uri: photo }} style={styles.image} />
+                </View>
+                <Text style={styles.imageTitle}>Завантажте фото</Text>
+                <KeyboardAvoidingView
+                    behavior={Platform.OS === "ios" ? "padding" : "height"}
+                >
+                    <TextInput style={styles.input} placeholder="Назва..." />
+                </KeyboardAvoidingView>
+                <View style={styles.geoInputWrapper}>
+                    <SvgXml xml={iconMarker} style={styles.marker} />
                     <KeyboardAvoidingView
                         behavior={Platform.OS === "ios" ? "padding" : "height"}
                     >
                         <TextInput
-                            style={styles.input}
-                            placeholder="Назва..."
+                            style={styles.geoInput}
+                            placeholder="Місцевість..."
                         />
                     </KeyboardAvoidingView>
-                    <View style={styles.geoInputWrapper}>
-                        <SvgXml xml={iconMarker} style={styles.marker} />
-                        <KeyboardAvoidingView
-                            behavior={
-                                Platform.OS === "ios" ? "padding" : "height"
-                            }
-                        >
-                            <TextInput
-                                style={styles.geoInput}
-                                placeholder="Місцевість..."
-                            />
-                        </KeyboardAvoidingView>
-                    </View>
-                    <BtnStyled onPress={onPublic} title="Опублікувати" />
                 </View>
-            </TouchableWithoutFeedback>
-            <View style={styles.footer}>
-                <Pressable
-                    style={styles.btnDeletePublication}
-                    onPress={() => navigation.navigate("Create post")}
-                >
-                    <SvgXml xml={iconRecicle} />
-                </Pressable>
+                <BtnStyled onPress={onPublic} title="Опублікувати" />
+                <View style={styles.footer}>
+                    <Pressable
+                        style={styles.btnDeletePublication}
+                        onPress={() => navigation.navigate("Posts")}
+                    >
+                        <SvgXml xml={iconRecicle} />
+                    </Pressable>
+                </View>
             </View>
-        </>
+        </TouchableWithoutFeedback>
     );
 };
 
 const styles = StyleSheet.create({
     container: {
+        position: "relative",
+        flex: 1,
         flexGrow: 1,
         paddingTop: 32,
         paddingHorizontal: 16,
         backgroundColor: "#fff",
+    },
+    cameraContainer: {
+        flex: 1,
+        flexGrow: 1,
+        position: "absolute",
+        top: 0,
+        left: 0,
+        width: Dimensions.get("window").width,
+        height: Dimensions.get("window").height - StatusBar.currentHeight - 55,
+        backgroundColor: "red",
+        alignItems: "center",
+        overflow: "hidden",
+        zIndex: 2,
+    },
+    camera: {
+        flex: 1,
+        height: "auto",
+        aspectRatio: 3 / 4,
+    },
+    btnWrapper: {
+        position: "absolute",
+        bottom: 0,
+        left: 0,
+        flexDirection: "row",
+        padding: 16,
+        justifyContent: "space-between",
+        width: Dimensions.get("window").width,
     },
     imageWrapper: {
         position: "relative",
@@ -80,15 +185,16 @@ const styles = StyleSheet.create({
         borderColor: "#E8E8E8",
         borderRadius: 8,
     },
-    camera: {
+    image: {
+        width: "100%",
+        height: 240,
+    },
+    btnCamera: {
         position: "absolute",
         top: "50%",
         left: "50%",
         transform: [{ translateX: -30 }, { translateY: -30 }],
-    },
-    image: {
-        width: "100%",
-        height: 240,
+        zIndex: 1,
     },
     imageTitle: {
         marginBottom: 32,
@@ -120,9 +226,11 @@ const styles = StyleSheet.create({
         borderColor: "#E8E8E8",
     },
     footer: {
-        flexGrow: 0,
+        position: "absolute",
+        bottom: 0,
+        left: 0,
         alignItems: "center",
-        width: "100%",
+        width: Dimensions.get("window").width,
         height: 58,
         paddingVertical: 9,
         backgroundColor: "#fff",
--- src/Screens/Home.js
+++ src/Screens/Home.js
@@ -63,110 +63,6 @@ const Home = () => {
                         );
                     }
                 },
-                // tabBarButton: () => {
-                //     if (route.name === "Posts") {
-                //         return (
-                //             <TouchableOpacity onPress={goHome}>
-                //                 <Svg
-                //                     width="40"
-                //                     height="40"
-                //                     viewBox="0 0 40 40"
-                //                     fill="none"
-                //                     xmlns="http://www.w3.org/2000/svg"
-                //                 >
-                //                     <Path
-                //                         fill-rule="evenodd"
-                //                         clip-rule="evenodd"
-                //                         d="M11 11H18V18H11V11Z"
-                //                         stroke="#212121"
-                //                         stroke-opacity="0.8"
-                //                         stroke-linecap="round"
-                //                         stroke-linejoin="round"
-                //                     />
-                //                     <Path
-                //                         fill-rule="evenodd"
-                //                         clip-rule="evenodd"
-                //                         d="M22 11H29V18H22V11Z"
-                //                         stroke="#212121"
-                //                         stroke-opacity="0.8"
-                //                         stroke-linecap="round"
-                //                         stroke-linejoin="round"
-                //                     />
-                //                     <Path
-                //                         fill-rule="evenodd"
-                //                         clip-rule="evenodd"
-                //                         d="M22 22H29V29H22V22Z"
-                //                         stroke="#212121"
-                //                         stroke-opacity="0.8"
-                //                         stroke-linecap="round"
-                //                         stroke-linejoin="round"
-                //                     />
-                //                     <Path
-                //                         fill-rule="evenodd"
-                //                         clip-rule="evenodd"
-                //                         d="M11 22H18V29H11V22Z"
-                //                         stroke="#212121"
-                //                         stroke-opacity="0.8"
-                //                         stroke-linecap="round"
-                //                         stroke-linejoin="round"
-                //                     />
-                //                 </Svg>
-                //             </TouchableOpacity>
-                //         );
-                //     }
-                //     if (route.name === "CreatePost") {
-                //         return (
-                //             <TouchableOpacity onPress={goCreatePost}>
-                //                 <View style={styles.btnCreatePublication}>
-                //                     <Svg
-                //                         width={25}
-                //                         height={25}
-                //                         viewBox="0 0 25 25"
-                //                         fill="none"
-                //                         xmlns="http://www.w3.org/2000/svg"
-                //                     >
-                //                         <Path
-                //                             fillRule="evenodd"
-                //                             clipRule="evenodd"
-                //                             d="M13 6H12V12H6V13H12V19H13V13H19V12H13V6Z"
-                //                             fill="#fff"
-                //                         />
-                //                     </Svg>
-                //                 </View>
-                //             </TouchableOpacity>
-                //         );
-                //     }
-                //     if (route.name === "Profile") {
-                //         return (
-                //             <TouchableOpacity onPress={goProfile}>
-                //                 <Svg
-                //                     width={40}
-                //                     height={40}
-                //                     viewBox="0 0 40 40"
-                //                     fill="none"
-                //                     xmlns="http://www.w3.org/2000/svg"
-                //                 >
-                //                     <Path
-                //                         d="M28 29V27C28 24.7909 26.2091 23 24 23H16C13.7909 23 12 24.7909 12 27V29"
-                //                         stroke="#212121"
-                //                         stroke-opacity="0.8"
-                //                         stroke-linecap="round"
-                //                         stroke-linejoin="round"
-                //                     />
-                //                     <Path
-                //                         fill-rule="evenodd"
-                //                         clip-rule="evenodd"
-                //                         d="M20 19C22.2091 19 24 17.2091 24 15C24 12.7909 22.2091 11 20 11C17.7909 11 16 12.7909 16 15C16 17.2091 17.7909 19 20 19Z"
-                //                         stroke="#212121"
-                //                         stroke-opacity="0.8"
-                //                         stroke-linecap="round"
-                //                         stroke-linejoin="round"
-                //                     />
-                //                 </Svg>
-                //             </TouchableOpacity>
-                //         );
-                //     }
-                // },
             })}
         >
             <FooterTabs.Screen
@@ -183,14 +79,6 @@ const Home = () => {
                     ),
                 }}
             />
-            {/* <FooterTabs.Screen
-                name="CreatePost"
-                component={CreatePostsScreen}
-                options={{
-                    title: "Створити публікацію",
-                    ...defaultOptions,
-                }}
-            /> */}
             <FooterTabs.Screen
                 name="Profile"
                 component={ProfileScreen}
